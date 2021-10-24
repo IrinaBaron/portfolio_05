37c5b643bed6da47bd7e5a386d200f9126e85710
Irina Baron <irr.baron@gmail.com>
2021-10-24 22:12:27 +0300

    changed files

--- src/js/main.js
+++ src/js/main.js
@@ -18,6 +18,61 @@ jsTriggers.forEach(function (trigger) { // 1
 });
 
 
+// accordion
+
+
+let accordion = (function (element) {
+  let _getItem = function (elements, className) { // функция для получения элемента с указанным классом
+    let element = undefined;
+    elements.forEach(function (item) {
+      if (item.classList.contains(className)) {
+        element = item;
+      }
+    });
+    return element;
+  };
+  return function () {
+    let _mainElement = {}, // .accordion
+      _items = {}, // .accordion-item
+      _contents = {}; // .accordion-item-content
+    let _actionClick = function (e) {
+      if (!e.target.classList.contains('accordion__item-header')) { // прекращаем выполнение функции если кликнули не по заголовку
+        return;
+      }
+      e.preventDefault(); // отменям стандартное действие
+      // получаем необходимые данные
+      let header = e.target,
+        item = header.parentElement,
+        itemActive = _getItem(_items, 'show');
+      if (itemActive === undefined) { // добавляем класс show к элементу (в зависимости от выбранного заголовка)
+        item.classList.add('show');
+      } else {
+        // удаляем класс show у ткущего элемента
+        itemActive.classList.remove('show');
+        // если следующая вкладка не равна активной
+        if (itemActive !== item) {
+          // добавляем класс show к элементу (в зависимости от выбранного заголовка)
+          item.classList.add('show');
+        }
+      }
+    },
+    _setupListeners = function () {
+      // добавим к элементу аккордиона обработчик события click
+      _mainElement.addEventListener('click', _actionClick);
+    };
+
+    return {
+      init: function (element) {
+        _mainElement = (typeof element === 'string' ? document.querySelector(element) : element);
+        _items = _mainElement.querySelectorAll('.accordion__item');
+        _setupListeners();
+      }
+    }
+  }
+})();
+
+let accordion1 = accordion();
+accordion1.init('#accordion');
 // burger-menu 
 
 // const burgerBtn = document.querySelector('.header__burger')
